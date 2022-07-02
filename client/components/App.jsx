8e873b2f8cbc185d92956a392f45df3302e6fd94
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-02 17:43:02 +1200

    Update App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,13 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+// import React, { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
 
-import { fetchFruits } from '../actions'
+// import { fetchFruits } from '../actions'
+
+import React from 'react'
 
 function App () {
-  const fruits = useSelector(state => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
-
   return (
-    <>
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <h1>Notes</h1>
   )
 }
 
