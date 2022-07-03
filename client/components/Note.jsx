3cc5fc0674f19e5da1c232f48357bd0a645f399d
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-03 20:53:23 +1200

    Add created date

--- client/components/Note.jsx
+++ client/components/Note.jsx
@@ -18,7 +18,7 @@ function Note(props){
   return(
     <div className='note'>
       <div className='note-header'>
-        <small>Date</small>
+        <small>{props.note.created_at}</small>
         <i className='delete-icon' onClick={handleDelete} id={props.note.id}><BsX/></i>
       </div>
       <div className='note-body'>
--- server/db/migrations/20220702052725_notes.js
+++ server/db/migrations/20220702052725_notes.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('notes', (table) => {
     table.increments('id')
     table.string('text')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
   })
 }
 
